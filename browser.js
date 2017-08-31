a8847a9e268ae53bd0394cccabf003dcb1a8c8bd
julianseibert <julianseibert@gmx.de>
2017-09-01 00:44:46 +0200

    late night save

--- browser.js
+++ browser.js
@@ -33,19 +33,54 @@ function Browser() {
             deferred.resolve();
         } else {
 
-
             const {exec} = require('child_process');
 
-
-
-            exec('export DISPLAY=:0 ; chromium-browser --no-sandbox --disable-translate --kiosk --incognito https://www.thing-it.com/', (err, stdout, stderr) => {
+            /**
+             * release running xServer
+             */
+            exec('rm /tmp/.X0-lock', (err, stdout, stderr) => {
+                //    exec('/usr/bin/X :0', (err, stdout, stderr) => {
                 if (err) {
                     console.error(`exec error: ${err}`);
                     return;
                 }
 
-                console.log(`Browser exit with:  ${stdout}`);
+                console.log(`xServer release:  ${stdout}`);
+
             });
+
+
+            /**
+             * lock new xServer on DISPLAY : 0
+             */
+//            setTimeout(function () {
+                exec('/usr/bin/X :0 -nocursor', (err, stdout, stderr) => {
+                    //    exec('/usr/bin/X :0', (err, stdout, stderr) => {
+                    if (err) {
+                        console.error(`exec error: ${err}`);
+                        return;
+                    }
+
+                    console.log(`xServer lock:  ${stdout}`);
+
+                });
+//            }, 2000);
+
+
+            /**
+             * Initialise Input devices
+             */
+//            setTimeout(function () {
+                exec('export DISPLAY=:0 ; chromium-browser --no-sandbox --disable-translate --kiosk --incognito --test-type https://www.thing-it.com/', (err, stdout, stderr) => {
+                    //exec('chromium-browser --no-sandbox --disable-translate --kiosk --incognito https://www.thing-it.com/', (err, stdout, stderr) => {
+                    if (err) {
+                        console.error(`exec error: ${err}`);
+                        return;
+                    }
+
+                    console.log(`Browser exit with:  ${stdout}`);
+                });
+ //           }, 2000);
 
 
             deferred.resolve();
